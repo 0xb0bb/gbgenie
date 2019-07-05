9004ede6164490a5d844e503da344eacb4259ae4
b0bb <nuny@bidnes.com>
2019-07-05 09:52:45 +0200

    initial commit

---
+++ gbgenie.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+import re
+
+
+def check(code):
+
+    if re.search(r"^[0-9A-Fa-f]{3}-[0-9A-Fa-f]{2}[8-9A-Fa-f]$", code):
+        return True
+
+    if re.search(r"^[0-9A-Fa-f]{3}-[0-9A-Fa-f]{2}[8-9A-Fa-f]-[0-9A-Fa-f]{3}$", code):
+        x = int(code[8], 16) ^ int(code[9], 16)
+        if x == 0 or x > 7:
+            return True
+
+    return False
+
+
+def rr8(n, shift):
+
+    return (n >> shift) | (n << (8 - shift)) & 0xff
+
+
+def rl8(n, shift):
+
+    return ((n << shift) | (n >> (8 - shift))) & 0xff
+
+
+def encode(val, addr, orig=None):
+
+    code = '%02X' % val
+    code += '%X-%02X%X' % ((addr & 0xf00) >> 8, addr & 0xff, 0xf - (addr >> 12))
+
+    if orig is not None:
+        num = rl8(orig ^ 0x45, 2) ^ 0xff
+        chk = ((num & 0xf0) >> 4) ^ 0x08
+
+        code += '-%X%X%X' % ((num & 0xf0) >> 4, chk, num & 0x0f)
+
+    return code
+
+
+def decode(code):
+
+    if check(code) is False:
+        return False
+
+    val  = int(code[:2], 16)
+    addr = (0xf - int(code[6], 16)) << 12 | int(code[2] + code[4] + code[5], 16)
+    orig = None
+
+    if len(code) > 7:
+        orig = rr8(int(code[8] + code[10], 16) ^ 0xff, 2) ^ 0x45
+
+    return {'addr': addr, 'value': val, 'original': orig}
+
+
+def usage(prog):
+
+    print 'usage:    %s <code>' % prog
+    print '          %s <addr> <value> [original]' % prog
+    print ''
+    print 'examples:\n'
+    print '\tdecode: %s 001-5CA-E62' % prog
+    print '\t        %s 001-5CA' % prog
+    print ''
+    print '\tencode: %s 0x7654 0x01' % prog
+    print '\t        %s 0x7654 0x02 0xff' % prog
+    print '\t        %s 1234 ab cd\n' % prog
+
+
+def main():
+
+    import sys
+
+    if len(sys.argv) < 2:
+        usage(sys.argv[0])
+        sys.exit(-1)
+
+    if len(sys.argv) == 2:
+        val = decode(sys.argv[1])
+        if val is False:
+            print 'error: invalid game genie code detected'
+            sys.exit(-1)
+
+        if val['original'] is not None:
+            print '0x%04x: 0x%02x => 0x%02x' % (val['addr'], val['original'], val['value'])
+        else:
+            print '0x%04x: 0x%02x' % (val['addr'], val['value'])
+
+    else:
+
+        addr = int(sys.argv[1], 16)
+        val  = int(sys.argv[2], 16)
+        orig = None
+
+        if len(sys.argv) > 3:
+            orig = int(sys.argv[3], 16)
+
+        print 'code: %s' % encode(val, addr, orig)
+
+
+if __name__ == '__main__':
+    main()
